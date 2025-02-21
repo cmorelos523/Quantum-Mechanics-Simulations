e30fa3dfdf4a6052bd62f3b4494135a10b05caf3
Carlos Alberto Morelos Escalera <169103268+cmorelos523@users.noreply.github.com>
2025-02-20 16:08:11 -0800

    Update CHSH-Inequality.py

--- CHSH-Inequality.py
+++ CHSH-Inequality.py
@@ -6,3 +6,14 @@ from qiskit_aer import Aer
 #from qiskit import execute
 from qiskit.visualization import plot_histogram
 
+# Measurement transformations:
+# For qubit 0, measuring in the X basis is achieved by applying a Hadamard (H).
+# For qubit 1, we transform into the basis of the observable.
+def XW(circ, qbit0, qbit1):
+    # Qubit 0: X measurement -> convert to Z via H
+    circ.h(qbit0)
+    # Qubit 1: W measurement -> convert to Z using S, H, T, H
+    circ.s(qbit1)
+    circ.h(qbit1)
+    circ.t(qbit1)
+    circ.h(qbit1)
