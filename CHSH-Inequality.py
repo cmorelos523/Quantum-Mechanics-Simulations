d5022e7cc801b71044412c138f30026c41563802
Carlos Alberto Morelos Escalera <169103268+cmorelos523@users.noreply.github.com>
2025-02-20 16:07:00 -0800

    Create CHSH-Inequality.py

---
+++ CHSH-Inequality.py
@@ -0,0 +1,8 @@
+# import necessary modules
+import numpy as np
+import matplotlib.pyplot as plt
+from qiskit import QuantumCircuit, transpile
+from qiskit_aer import Aer
+#from qiskit import execute
+from qiskit.visualization import plot_histogram
+
